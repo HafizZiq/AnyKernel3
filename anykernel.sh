51bb47b6886a6420811500033635dca50008e5ae
Hafiz <hafizuddinismail552@gmail.com>
2021-01-03 20:44:40 +0800

    switch to PerawanX

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Tebhoc☭ Kernel by HafizZiq
+kernel.string=PerawanX☭ Kernel by HafizZiq
 do.devicecheck=1
 do.modules=1
 do.systemless=0
