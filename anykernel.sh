ea4a66fff1102716799a4d92807911c9c518fbe5
HafizZiq <hafizuddinismail552@gmail.com>
2021-03-20 19:32:56 +0800

    anykernel.sh: update it sur 1. we dont use modules 2. no fuckin custom ramdisk
    Signed-off-by: HafizZiq <hafizuddinismail552@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -6,7 +6,7 @@
 properties() { '
 kernel.string=PerawanX☭ Kernel by HafizZiq
 do.devicecheck=1
-do.modules=1
+do.modules=0
 do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
@@ -24,17 +24,9 @@ block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
-
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
-
-
-## AnyKernel file attributes
-# set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-
 
 ## AnyKernel install
 dump_boot;
