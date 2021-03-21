a3282ba05f93845fca3806b083fd50d75968b835
HafizZiq <hafizuddinismail552@gmail.com>
2021-03-21 15:57:44 +0800

    ak3: invictus boi
    Signed-off-by: HafizZiq <hafizuddinismail552@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Boob Kernel (onlyfans Edition) By Fiz
+kernel.string=InvictusRamen⚡️ By HafizZiq
 do.devicecheck=1
 do.modules=0
 do.systemless=0
