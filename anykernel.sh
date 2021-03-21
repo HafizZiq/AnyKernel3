651c99faa65032edede6bd60aebbc29aa1c1091f
HafizZiq <hafizuddinismail552@gmail.com>
2021-03-21 15:25:57 +0800

    inital onlyfans boi
    Signed-off-by: HafizZiq <hafizuddinismail552@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,24 +4,22 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=SixTeen Kernel by HafizZiq
+kernel.string=Boob Kernel (onlyfans Edition) By Fiz
 do.devicecheck=1
 do.modules=0
 do.systemless=0
 do.cleanup=1
-do.cleanuponabort=0
-device.name1=ginkgo
-device.name2=willow
-device.name3=
-device.name4=
-device.name5=
-supported.versions=10 - 11
+do.cleanuponabort=1
+device.name1=ugglite
+device.name2=riva
+device.name3=ugg
+supported.versions=
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=0;
+block=auto;
+is_slot_device=auto;
 ramdisk_compression=auto;
 
 ## AnyKernel methods (DO NOT CHANGE)
