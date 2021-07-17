715ad216b36cbb1e59cc82c2b1419d981cd6df9b
HafizZiq <hafizuddinismail552@gmail.com>
2021-07-18 04:20:49 +0800

    ak3: Add support for Android S
    and dont let'em flashing it on Q lmao
    
    Signed-off-by: HafizZiq <hafizuddinismail552@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=SixTeen Kernel by HafizZiq
+kernel.string=SixTeen Kernel (R/S) by HafizZiq
 do.devicecheck=1
 do.modules=0
 do.systemless=0
@@ -15,7 +15,7 @@ device.name2=willow
 device.name3=
 device.name4=
 device.name5=
-supported.versions=10 - 11
+supported.versions=11 - 12
 supported.patchlevels=
 '; } # end properties
 
